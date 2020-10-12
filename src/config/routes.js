1aae008bb9f767f9380e0bc5e1de04216d6a6aef
landita <landainitpy@gmail.com>
2020-10-11 21:32:19 -0600

    agregacion de componentes login, navbar

--- src/config/routes.js
+++ src/config/routes.js
@@ -6,15 +6,16 @@ import {
   Link
 } from "react-router-dom";
 //import de componentes
+import NavbarComponent from '../components/navbar/navbar';
 import LoginComponent from "../components/login/login";
 
 const RouterLinks = () => {
     return(
         <Router>
             <Switch>
-                {/* imports del dashboard */}
-            </Switch>
-            <Switch>
+                <Route path="/" exact component={NavbarComponent}>
+                    
+                </Route>
                 <Route path="/login" component={LoginComponent} />
             </Switch>
         </Router>

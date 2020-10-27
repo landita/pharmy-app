de40a27ba8e8eee4f2c7849c8fd4a7cfa865a942
carloshdz99 <carloshdzfunes@gmail.com>
2020-10-27 16:55:34 -0600

    reactstrap and bootstrap

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import {FirebaseAppProvider} from 'reactfire';
 import firebaseConfig from './firebase.config';
 import DashboardComponent from './components/dashboard';
+//importando bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <FirebaseAppProvider firebaseConfig={firebaseConfig}>
